c05c616cd68d9dd272968a0d81321d1fa20f30dd
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-06-11 15:31:34 +0200

    Adds zugmap ortsplan and orthofotoplus layers

---
+++ collective/geo/zugmap/maplayers.py
@@ -0,0 +1,12 @@
+from collective.geo.mapwidget.maplayers import MapLayer
+from collective.geo.zugmap import _
+
+class ZugMapOrtsplanLayer(MapLayer):
+    name = u"zugmap_ortsplan"
+    Title = _(u"Zugmap Ortsplan")
+    type = 'zugmap'
+
+class ZugMapOrthofotoPlus(MapLayer):
+    name = u"zugmap_orthofotoplus"
+    Title = _(u"Zugmap Orthofotoplus")
+    type = 'zugmap'
