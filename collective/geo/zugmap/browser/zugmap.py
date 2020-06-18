a92b5e0c21558dce7241aefe62864d288949e9f9
Matthias Osswald <m@osswald.li>
2020-06-18 11:27:50 +0200

    Implement both layers using base class.

--- collective/geo/zugmap/browser/zugmap.py
+++ collective/geo/zugmap/browser/zugmap.py
@@ -2,29 +2,41 @@ from Products.Five.browser import BrowserView
 import requests
 
 
-class ZugMap(BrowserView):
+class ZugMapBase(BrowserView):
+
+    def __init__(self, context, request):
+        super(ZugMapBase, self).__init__(context, request)
+        self.name = ""
+        self.layer = ""
+        self.style = ""
+        self.matrixSet = ""
+
     def __call__(self):
+        return self._get_layer_js()
+
+    def _get_layer_js(self):
         js_string = '''
             function() {
-                /* https://github.com/Safecast/GeoSense/blob/master/public/lib/openlayers/OpenLayers-2.13.1/tests/Layer/WMTS.html */
-                /* https://github.com/Safecast/GeoSense/blob/19d4c01999a06c061a94f7d9a6c98d2916e44526/public-build/lib/openlayers/OpenLayers-2.13.1/tests/Layer/WMTS.html#L205-L214 */
-
+                var name = `%s`;
+                var layer = `%s`;
+                var style = `%s`;
+                var matrixSet = `%s`;
                 var html = `%s`;
+
                 var doc = new OpenLayers.Format.XML().read(html);
                 var obj = new OpenLayers.Format.WMTSCapabilities().read(doc);
 
-                var wmts_layer = "zg.ortsplan";
                 var url_template = obj.contents.layers.find(
-                    x => x.identifier === wmts_layer
+                    x => x.identifier === layer
                 ).resourceUrls[0].template;
 
                 var layer = new OpenLayers.Layer.WMTS({
-                    name: "Ortsplan des Kantons Zug",
+                    name: name,
                     requestEncoding: "REST",
                     url: url_template,
-                    layer: wmts_layer,
-                    style: "default",
-                    matrixSet: "zg",
+                    layer: layer,
+                    style: style,
+                    matrixSet: matrixSet,
                     format: "image/png",
                     matrixIds: obj.contents.tileMatrixSets["zg"].matrixIds,
                     'attribution': '<span style="color: white; background-color: #333;">Quelle: GIS Kanton Zug</span>'
@@ -32,10 +44,28 @@ class ZugMap(BrowserView):
 
                 return layer;
             }
-            ''' % (self._get_capabilities())
+            ''' % (self.name, self.layer, self.style, self.matrixSet, self._get_capabilities())
         return js_string
 
     def _get_capabilities(self):
         url = "https://services.geo.zg.ch/tc/wmts/1.0.0/WMTSCapabilities.xml"
         response = requests.get(url)
         return response.text
+
+
+class ZugMapOrtsplan(ZugMapBase):
+    def __init__(self, context, request):
+        super(ZugMapOrtsplan, self).__init__(context, request)
+        self.name = "Ortsplan des Kantons Zug"
+        self.layer = "zg.ortsplan"
+        self.style = "default"
+        self.matrixSet = "zg"
+
+
+class ZugMapOrthoPlus(ZugMapBase):
+    def __init__(self, context, request):
+        super(ZugMapOrthoPlus, self).__init__(context, request)
+        self.name = "LuftbildPLUS"
+        self.layer = "zg.luftbildplus"
+        self.style = "default"
+        self.matrixSet = "zg"
